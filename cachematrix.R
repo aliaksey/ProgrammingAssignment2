dea134e62f8d66b358aec61c4b98f8350f151a86
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-23 16:30:59 +0100

    cleaning a bit and start coding!!!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Function that creates list of functions that are able to 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#set the value of the inverted matrix
+  i <- NULL
